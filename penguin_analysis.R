94521c8d63252d36336c01f3547a522e059ba762
Elle O'Brien <elleobrien11@gmail.com>
2020-10-22 16:13:36 -0700

    Add files via upload

---
+++ penguin_analysis.R
@@ -0,0 +1,31 @@
+library(palmerpenguins)
+library(ggplot2)
+library(kableExtra)
+library(broom)
+
+
+data(package = 'palmerpenguins')
+
+
+# Look at distribution of bill lengths in 3 species
+palette = c("#999999", "#E69F00", "#56B4E9")
+theme_set(
+  theme_classic(base_size = 25)
+)
+
+px <- ggplot(data=penguins, aes(bill_length_mm))+
+  geom_histogram(aes(fill=species), alpha=0.6, position="identity")+
+  theme_classic()+
+  xlab("Bill Length (mm)")+
+  ylab("Count")+
+  ggtitle("Penguins noses")+
+  scale_fill_manual("Species",values=palette)
+px  
+ggsave("my_penguin_plot.png",px, width=4,height=4)
+
+# Run a linear model
+
+model <- lm(bill_length_mm ~ 1 + species, data = penguins) %>%
+  tidy() %>%
+  kable() %>%
+  cat(.,file="mymodel.html")
