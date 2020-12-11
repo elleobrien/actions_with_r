02e1cc9892be79d2195ec8bd578aa77a27eacdfd
Elle O'Brien <elleobrien11@gmail.com>
2020-12-11 13:45:31 -0800

    Update penguin_analysis.R

--- penguin_analysis.R
+++ penguin_analysis.R
@@ -4,13 +4,9 @@ library(dplyr)
 library(kableExtra)
 
 data(package = 'palmerpenguins')
-id <- "1Ut38XjBahKE7ess-Dgm0202PaHZNbcD7" 
-new <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
-penguins <- rbind(penguins, new)
 
-
-# Look at distribution of bill lengths in 3 species
-palette = c("#999999", "#E69F00", "#56B4E9", "#00FF00")
+# Look at distribution of bill lengths in each species
+palette = c("#999999", "#E69F00", "#56B4E9", "#643A71")
 theme_set(
   theme_classic(base_size = 7)
 )
@@ -27,7 +23,7 @@ px <- ggplot(data=penguins, aes(bill_length_mm))+
 px  
 ggsave("my_penguin_plot.png",px, device = "png", width=3,height=1.5,dpi=300)
 
-# Run a linear model
+# Report average measures of beak/body size per species
 t <- penguins %>%
   select(c("species","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g")) %>%
   aggregate(.~species,.,FUN=mean,na.rm=TRUE) %>%
